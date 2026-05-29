26f385b08e6a2bb61b09f3c3ec7b54ec54e1fd10
ERICK GABRIEL <erickmonteiro3000@gmail.com>
2026-05-29 15:28:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-
 const horasElemento = document.getElementById('horas');
 const minutosElemento = document.getElementById('minutos');
 const segundosElemento = document.getElementById('segundos');
-
 
 const relogio = setInterval(function time() {
     let dateToday = new Date();
@@ -10,13 +8,14 @@ const relogio = setInterval(function time() {
     let min = dateToday.getMinutes();
     let seg = dateToday.getSeconds();
 
+    // Adiciona o '0' na frente se o número for menor que 10
     let h = (horas < 10) ? '0' + horas : horas;
     let m = (min < 10) ? '0' + min : min;
     let s = (seg < 10) ? '0' + seg : seg;
 
-
+    // Atualiza o texto na tela
     horasElemento.textContent = h;
     minutosElemento.textContent = m;
     segundosElemento.textContent = s;
 
-}, 1000); 
+}, 1000);
